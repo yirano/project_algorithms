a83bccd4b954879b5e29642104d1d5c45f66444d
yirano <yirano@protonmail.ch>
2020-10-04 15:36:54 -0400

    Completed single_number

--- single_number/test_single_number.py
+++ single_number/test_single_number.py
@@ -2,15 +2,16 @@ import unittest
 import random
 from single_number import single_number
 
+
 class Test(unittest.TestCase):
     def test_single_number(self):
         arr = []
-
         for i in range(1000):
             arr.append(i)
             arr.append(i)
+        print(arr)
 
-        random.shuffle(arr)
+        # random.shuffle(arr)
         rand_index = random.randint(0, len(arr))
         num = arr.pop(rand_index)
 

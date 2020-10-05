a9a090cad2c16af6ad09de3639d208f53fbdb3e0
yirano <yirano@protonmail.ch>
2020-10-05 16:56:01 -0400

    Another sliding_window_max -- still not passing th large test though

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,7 +4,12 @@ Returns: a List of integers
 '''
 
 
-def sliding_window_max(nums, k):
+import collections
+import time
+
+
+# ! 20.163s
+def sliding_window_max_1(nums, k):
     # Your code here
     container = []
     for i in range(0, len(nums)-(k-1)):
@@ -12,12 +17,32 @@ def sliding_window_max(nums, k):
     return container
 
 
-# arr = [1, 3, -1, -3, 5, 3, 6, 7]
-# k = 3
+# ! 20.257s
+# def sliding_window_max_2(nums, k):
+#     # Your code here
+#     d = Deque()
+#     for i in range(0, len(nums)-(k-1)):
+#         d.append(max(nums[i:i+k]))
+#     return list(collections.deque((d)))
 
-# print(sliding_window_max(arr, k))
+
+def sliding_window_max(nums, k):
+    c = []
+    i = 0
+    # c.append(max)
+    while k < len(nums)+1:
+        c.append(max(nums[0:k]))
+        nums.pop(0)
+        # sliding_window_max(nums, k, c)
+    return c
 
 
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+start = time.time()
+print(sliding_window_max(arr, k))
+end = time.time()
+print(end-start)
 '''
 if __name__ == '__main__':
     # Use the main function here to test out your implementation

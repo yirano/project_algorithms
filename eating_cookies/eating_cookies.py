9202c2abae4df9789873a06f01a858877d632ed5
yirano <yirano@protonmail.ch>
2020-10-05 21:50:48 -0400

    Eating cookies small number passing -- passing a second argument breaks the code

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,27 @@
 Input: an integer
 Returns: an integer
 '''
+
+
 def eating_cookies(n):
-    # Your code here
+    dict = [1, 1, 2, 4]
+    total = 0
+    i = 4
+    if n < 4:
+        return dict[n]
+    else:
+        while i <= n:
+            dict.append(sum(dict[i-3:i]))
+            i += 1
 
-    pass
+    return dict[len(dict)-1]
 
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+print(eating_cookies(50))
+
+# if __name__ == "__main__":
+#     # Use the main function here to test out your implementation
+#     num_cookies = 5
+
+#     print(
+#         f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")

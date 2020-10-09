b1440a7a337e8e7763668fae0f273a85ff92cb8b
yirano <yirano@protonmail.ch>
2020-10-08 21:46:12 -0400

    Added Adam's code

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -17,7 +17,30 @@ def eating_cookies(n, arr=[]):
     return dict[len(dict)-1]
 
 
-print(eating_cookies(500))
+def eating_cookies_1(n, arr=[]):
+    arr = [1, 2, 3]
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+    ct = 0
+    for i in range(len(arr)):
+        ct += eating_cookies(n - arr[i])
+    return ct
+
+
+# Courtesy of Adam Young
+def eating_cookies(n, cache):
+    if n == 0:
+        cache[n] = 1
+    if n == 1 or n == 2:
+        cache[n] = n
+    if cache[n] == 0:
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
+
+# print(eating_cookies(500))
 
 # if __name__ == "__main__":
 #     # Use the main function here to test out your implementation
